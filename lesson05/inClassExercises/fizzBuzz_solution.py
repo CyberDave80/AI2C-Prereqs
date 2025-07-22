988dde28cfb379b865799d5e0401112994fd97c0
Ryan-L-N <thenevilletrove@gmail.com>
2025-07-22 12:47:58 -0400

    Lesson05 practical exercises and solutions

--- lesson05/inClassExercises/fizzBuzz_solution.py
+++ lesson05/inClassExercises/fizzBuzz_solution.py
@@ -1,3 +1,8 @@
+#Print the numbers from 1 to 100 except...
+#If the number is divisible by 3, print "Fizz"
+#If the number is divisible by 5, print "Buzz"
+#If the number is divisible by 3 and 5, print "Fizz Buzz"
+
 for number in range(1,101):
     if number % 3 == 0 and number % 5 == 0:
         print("Fizz Buzz")
